2d451dd46d75e590d84a52a6dab4a515753a6a61
Diego Armando <fordiego44@gmail.com>
2025-05-23 22:39:12 -0500

    seccion 97 - spring security

---
+++ src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.bolsadeideas.springboot.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain; 
+import com.bolsadeideas.springboot.app.auth.handler.LoginSuccessHandler;
+import com.bolsadeideas.springboot.app.models.service.JpaUserDetailsService;
+
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@Configuration
+public class SpringSecurityConfig {
+	 
+    @Autowired
+    private LoginSuccessHandler successHandler;
+    
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+  
+    @Autowired
+    private JpaUserDetailsService userDetailService;
+
+    @Autowired
+    public void userDetailsService(AuthenticationManagerBuilder build) throws Exception {
+       build.userDetailsService(userDetailService)
+       .passwordEncoder(passwordEncoder);
+    }
+  
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { 
+     return http
+             .authorizeHttpRequests(
+                 (authz) -> authz
+                     .requestMatchers("/", "/css/**", "/js/**", "/images/**", "/listar")
+                     .permitAll()
+                     .anyRequest().authenticated()
+             )
+             .formLogin(login -> login
+                 .successHandler(successHandler)
+                 .loginPage("/login")
+                 .permitAll())
+             .logout(logout -> logout.permitAll())
+             .exceptionHandling(ex -> ex.accessDeniedPage("/error_404"))
+             .build();
+     }
+}
